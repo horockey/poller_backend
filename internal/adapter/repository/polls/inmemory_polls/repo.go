119e8afc6641b737a4d142f0a086813d2265acfd
horockey <rassudov99@yandex.ru>
2023-11-20 19:29:43 +0300

    feat: polls inmemory impl

--- internal/adapter/repository/polls/inmemory_polls/repo.go
+++ internal/adapter/repository/polls/inmemory_polls/repo.go
@@ -1,6 +1,8 @@
 package inmemory_polls
 
 import (
+	"sync"
+
 	"github.com/horockey/poller_backend/internal/adapter/repository"
 	"github.com/horockey/poller_backend/internal/adapter/repository/polls"
 	"github.com/horockey/poller_backend/internal/model"
@@ -9,6 +11,8 @@ import (
 var _ polls.Repository = &inmemoryPolls{}
 
 type inmemoryPolls struct {
+	mu sync.RWMutex
+
 	storage map[string]*model.Poll
 }
 
@@ -19,6 +23,9 @@ func New() *inmemoryPolls {
 }
 
 func (repo *inmemoryPolls) Get(id string) (*model.Poll, error) {
+	repo.mu.RLock()
+	defer repo.mu.RUnlock()
+
 	poll, found := repo.storage[id]
 	if !found {
 		return nil, repository.ErrNotFound
@@ -28,16 +35,42 @@ func (repo *inmemoryPolls) Get(id string) (*model.Poll, error) {
 }
 
 func (repo *inmemoryPolls) GetAll() ([]*model.Poll, error) {
-	// TODO
-	return nil, nil
+	repo.mu.RLock()
+	defer repo.mu.RUnlock()
+
+	res := make([]*model.Poll, 0, len(repo.storage))
+	for _, poll := range repo.storage {
+		res = append(res, poll)
+	}
+
+	return res, nil
 }
 
 func (repo *inmemoryPolls) Add(p *model.Poll) (*model.Poll, error) {
-	// TODO
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
+	polls.FixID(p)
+
+	if _, found := repo.storage[p.ID]; found {
+		return nil, repository.ErrAlreadyExists
+	}
+
+	repo.storage[p.ID] = p
+
 	return nil, nil
 }
 
 func (repo *inmemoryPolls) Delete(id string) (*model.Poll, error) {
-	// TODO
-	return nil, nil
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+
+	poll, found := repo.storage[id]
+	if !found {
+		return nil, repository.ErrNotFound
+	}
+
+	delete(repo.storage, id)
+
+	return poll, nil
 }
